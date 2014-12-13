5b0b175c615b1caecad845a9b8d27fe254a249e6
Rajan Gupta <rajanjamu@gmail.com>
2014-12-13 20:10:49 +0530

    changed the path after facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def login
   	# http://3adly.blogspot.in/2012/12/ruby-on-rails-facebook-application.html
-  	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/')
+  	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + dashboard_path)
     @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream publish_stream")  
  	  redirect_to @auth_url
   end
