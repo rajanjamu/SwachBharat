fa4b476f4f09a4deb03cfb4dfe64e2480018648b
Anvesh <anvesh.v@gmail.com>
2014-12-13 14:21:45 +0530

    Schema Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212163024) do
+ActiveRecord::Schema.define(version: 20141213050130) do
+
+  create_table "cleans", force: true do |t|
+    t.integer  "user_id"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "aphoto"
+    t.string   "bphoto"
+  end
 
   create_table "marks", force: true do |t|
     t.integer  "user_id"
