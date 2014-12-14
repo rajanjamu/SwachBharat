ed782684b084a859ce516fc63968a86c59feda04
Anvesh <anvesh.v@gmail.com>
2014-12-14 14:50:03 +0530

    I dont know why there are still conflicts after pulling, successfully merging and pushing

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # get 'marks/index'
 
   # get 'marks/new'
-<<<<<<< HEAD
+
 
 
 
@@ -33,11 +33,11 @@ Rails.application.routes.draw do
   resources :marks
 
   get 'login' => 'sessions#login'
-=======
+
   # root to: 'home#sample'
   resources :marks
   # get 'login' => 'login#login'
->>>>>>> 8c06bd09a906f59270b457a361a3bcb0c6142045
+
   get 'dashboard' => 'home#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
