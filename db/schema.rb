da7e8703a79e6bafc65085423035d50f1750c07b
B Srikavya Ram <srikavyaram@gmail.com>
2014-12-15 11:01:14 +0530

    created mark tab

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214031123) do
+ActiveRecord::Schema.define(version: 20141215041820) do
 
   create_table "cleans", force: true do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141214031123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "photo"
+    t.text     "message"
   end
 
   create_table "users", force: true do |t|
