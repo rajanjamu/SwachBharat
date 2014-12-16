3c9e068b93c8eec4689adc71c819b46708bff4aa
Anvesh <anvesh.v@gmail.com>
2014-12-16 13:35:07 +0530

    corrected error in send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = swachbharat.herokuapp.com'
+  host = 'swachbharat.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
