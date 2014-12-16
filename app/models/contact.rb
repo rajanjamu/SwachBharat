e0736dc6aa54b1f971fa17b4355f549bf2c3e775
Anvesh <anvesh.v@gmail.com>
2014-12-16 16:11:07 +0530

    contact css

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Swachh Bharath",
       :to => "anvesh.v@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
