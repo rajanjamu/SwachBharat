181124bd82135ca7643c1e5ec6d87d3d04a32c9c
Rajan Gupta <rajanjamu@gmail.com>
2014-12-16 13:46:09 +0530

    updated production facebook app_id and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '873875205977099', '01ef8db432069f7da528adf459dcca02', :scope => 'email', :display => 'popup'
+  provider :facebook, app_id, secret, :scope => 'email', :display => 'popup'
 end
